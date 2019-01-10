80522316d2e049843c87179ee321bc5ae57aaa9a
MD Al-Hasan Mridha <mridha@anyconnect.com>
2019-01-10 18:00:09 +0600

    Let me count the ways

---
+++ count.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int M = 1000000007;
+int gcd_extended(int a, int b, int *x, int *y){
+	if(a==0){
+		x=1;
+		y=0;
+		return a;
+	}
+	int g=gcd_extended(b%a,a,&x1,&y1);
+	//x=
+	//y=
+	return g;
+}
+int mod_inverse(int a){
+	int x,y;
+	int g=
+}
+int comb(int n, int m){
+	//
+}
+int main(){
+	int t,n,m;
+	cin>>t;
+	for(int Case=1;Case<=t;Case++){
+		cin>>n>>m;
+		for(int i=0;i<=m;i++){
+			//
+		}
+	}
+}
