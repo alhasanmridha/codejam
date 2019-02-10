45e1f0e1a1599dfe1b0fabe2f5058a9e88a3f7ff
MD Al-Hasan Mridha <mdalhasanmridha@gmail.com>
2019-02-11 00:18:25 +0600

    Create Problem B. Lucky Dip(O(nk)).cpp

---
+++ Kickstart Round A 2018/Problem B. Lucky Dip(O(nk)).cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+double max(double a, double b){
+    return a>b?a:b;
+}
+int main(){
+    int tn;
+    cin>>tn;
+    for(int tc=1;tc<=tn;tc++){
+        int n,k;
+        cin>>n>>k;
+        double v[n];
+        for(int i=0;i<n;i++)
+        scanf("%lf",v+i);
+        double e0=0;
+        for(int i=0;i<k+1;i++){
+            double sum=0;
+            for(int j=0;j<n;j++){
+                sum = sum + max((double)v[j],e0);
+
+			}
+            e0=sum/(double)n;
+        }
+        cout<<"Case #"<<tc<<": "<<setprecision(7)<<fixed<<e0<<"\n";
+    }
+}
