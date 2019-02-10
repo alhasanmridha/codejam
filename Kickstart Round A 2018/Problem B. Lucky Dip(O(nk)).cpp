81a947be0d61e2cf58f25b0c9753284e71aff1ee
MD Al-Hasan Mridha <mdalhasanmridha@gmail.com>
2019-02-11 00:42:01 +0600

    update complexity from O(nk) to O(n logk)

--- Kickstart Round A 2018/Problem B. Lucky Dip(O(nk)).cpp
+++ Kickstart Round A 2018/Problem B. Lucky Dip(O(nk)).cpp
@@ -1,26 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
-double max(double a, double b){
-    return a>b?a:b;
-}
-int main(){
-    int tn;
-    cin>>tn;
-    for(int tc=1;tc<=tn;tc++){
-        int n,k;
-        cin>>n>>k;
-        double v[n];
-        for(int i=0;i<n;i++)
-        scanf("%lf",v+i);
-        double e=0;
-        for(int i=0;i<k+1;i++){
-            double sum=0;
-            for(int j=0;j<n;j++){
-                sum = sum + max((double)v[j],e);
 
-			}
-            e=sum/(double)n;
-        }
-        cout<<"Case #"<<tc<<": "<<setprecision(7)<<fixed<<e<<"\n";
-    }
+int main(){
+	int tn;
+	cin>>tn;
+	for(int tc=1;tc<=tn;tc++){
+		int n,k;
+		cin>>n>>k;
+		double v[n];
+		double vs[n+1];
+		vs[0]=0;
+		for(int i=0;i<n;i++){ 
+			scanf("%lf",v+i);
+		}
+		sort(v,v+n);
+		for(int i=0;i<n;i++){
+			vs[i+1]=vs[i]+v[i];
+		}
+		double e=0;
+		for(int i=0;i<k+1;i++){
+			int x = upper_bound(v,v+n,e)-v;
+			e=(x*e+vs[n]-vs[x])/(double)n;
+		}
+		cout<<"Case #"<<tc<<": "<<setprecision(7)<<fixed<<e<<"\n";
+	}
+	return 0;
 }
