cff572380dd6be4a79feea9da20cf9cf00288dd0
MD Al-Hasan Mridha <mdalhasanmridha@gmail.com>
2021-02-17 19:42:16 +0600

    Update Problem C. Let Me Count The Ways.cpp

--- Kick Start/Kickstart Round H 2018/Problem C. Let Me Count The Ways.cpp
+++ Kick Start/Kickstart Round H 2018/Problem C. Let Me Count The Ways.cpp
@@ -1,60 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-
-typedef long long LL;
-typedef unsigned long long ULL;
-
-const int N = 2 * 100000;
-LL fct[N + 5];
-LL po[N + 5];
-LL inv[N + 5];
-const LL MOD = 1e9 + 7;
-
-LL fast(LL A,LL B) {
-    LL ans = 1;
-    while(B) {
-        if(B & 1) ans = ans * A % MOD;
-        A = A * A % MOD;
-        B /= 2;
-    }
-    return ans;
+#define N 200001
+#define mod 1000000007
+typedef long long int LL;
+LL fact[N],p[N];
+LL mult(LL a, LL b){
+	return a*b%mod;
 }
-
-void pre() {
-    fct[0] = 1;
-    for(LL i = 1;i <= N;i++) fct[i] = fct[i-1] * i % MOD;
-    inv[N] = fast(fct[N], MOD - 2);
-    for(LL i = N - 1;i >= 0;i--) inv[i] = inv[i + 1] * (i + 1) % MOD;
-    po[0] = 1;
-    for(int i = 1;i <= N;i++) po[i] = po[i-1] * 2 % MOD;
-}
-
-LL combin(int n,int r) {
-    return fct[n] * inv[r] % MOD * inv[n - r] % MOD;
-}
-
-
-int main(){
-    pre();
-	int ntc;
-	cin>>ntc;
-	int n,m;
-	for(int tc = 1;tc <= ntc;tc++) {
-		cin>>n>>m;
-		
-		LL ways = 0;
-		
-		 for(int i = 0;i <= m;i++) {
-		     LL subways = combin(m,i) * po[i] % MOD * fct[2*n - i] % MOD;
-		     if(i & 1) ways = (ways + MOD - subways) % MOD;
-		     else ways = (ways + subways) % MOD;
-		 } 
-		cout<<"Case #"<<tc<<": "<<ways<<endl;
+LL inv(LL a){
+	LL e=mod-2;
+	LL ans=1;
+	while(e){
+		if(e&1) ans=mult(ans,a);
+		a=mult(a,a);
+		e>>=1;
 	}
-	
-	return 0;
+	return ans;
 }
-
-
-
+LL nck(LL n, LL k){
+	return mult(fact[n],inv(mult(fact[k],fact[n-k])));
+}
+int solve(){
+	int n,m;
+	scanf("%d%d",&n,&m);
+	LL ans=0,sn=1;
+	for(int i=0;i<=m;i++){
+		ans=(ans+sn*mult(nck(m,i),mult(fact[2LL*n-i],p[i])))%mod;
+		sn*=-1LL;
+	}
+	return (ans+mod)%mod;
+}
+int main(){
+	int t,nt=0;
+	fact[0]=p[0]=1LL;
+	for(LL i=1;i<N;i++) fact[i]=mult(fact[i-1],i),p[i]=mult(p[i-1],2LL);
+	scanf("%d",&t);
+	while(++nt<=t) cout<<"Case #"<<nt<<": "<<solve()<<"\n";
+}
